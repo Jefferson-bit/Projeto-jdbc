0974905e3b7f0416ef21118192c50361cbcf5dda
jefferson-bit <jhonsonbabi@hotmail.com>
2020-03-26 09:03:00 -0300

    Delete implementation

--- src/model/Dao/SellerDaoJDBC.java
+++ src/model/Dao/SellerDaoJDBC.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -28,47 +30,77 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
-       PreparedStatement st = null;
+        PreparedStatement st = null;
         try {
             st = con.prepareStatement(
                     "INSERT INTO seller "
-                            +"(Name,Email,BirthDate,BaseSalary,DepartmentId)"
-                            + "VALUES"
-                            +"(?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
+                    + "(Name,Email,BirthDate,BaseSalary,DepartmentId)"
+                    + "VALUES"
+                    + "(?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
             st.setString(1, obj.getName());
             st.setString(2, obj.getEmail());
             st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
             st.setDouble(4, obj.getBaseSalary());
             st.setInt(5, obj.getDepartment().getId());
-            
+
             int rowsAffected = st.executeUpdate();
-            
-            if(rowsAffected >0 ){
+
+            if (rowsAffected > 0) {
                 ResultSet rs = st.getGeneratedKeys();
-                if(rs.next()){
+                if (rs.next()) {
                     int id = rs.getInt(1);
                     obj.setId(id);
                 }
                 Conexao.closeResultSet(rs);
-            }else{
+            } else {
                 throw new DBException("unexcepted erro! no rows affected");
             }
         } catch (SQLException ex) {
             throw new DBException(ex.getMessage());
-        }finally{
+        } finally {
             Conexao.closeStatement(st);
         }
-                
-    }   
+
+    }
 
     @Override
     public void update(Seller obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        try {
+            st = con.prepareStatement(
+                    "UPDATE seller SET"
+                    + "Name=?,Email=?,BirthDate=?,BaseSalary=?,DepartmentId=?"
+                    + "WHERE Id=?");
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+
+        } catch (SQLException ex) {
+            throw new DBException("Erro ao atualizar dados");
+        } finally {
+            Conexao.closeStatement(st);
+        }
+
     }
 
     @Override
     public void deleteById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement st = null;
+        try {
+            st = con.prepareStatement("DELETE FROM seller WHERE id =? ");
+            st.setInt(1, id);
+            st.executeUpdate();
+            
+        } catch (SQLException ex) {
+            throw new DBException("Erro ao excluir");
+        }finally{
+            Conexao.closeStatement(st);
+        }
     }
 
     @Override
@@ -110,9 +142,9 @@ public class SellerDaoJDBC implements SellerDao {
                     + "FROM seller inner join department "
                     + "on seller.DepartmentId = department.id "
                     + "order by name");
-        
+
             rs = st.executeQuery();
-          List<Seller> list = new ArrayList<>();
+            List<Seller> list = new ArrayList<>();
             Map<Integer, Department> map = new HashMap<>();
             while (rs.next()) {
 
@@ -189,6 +221,5 @@ public class SellerDaoJDBC implements SellerDao {
         }
 
     }
-
 
 }
